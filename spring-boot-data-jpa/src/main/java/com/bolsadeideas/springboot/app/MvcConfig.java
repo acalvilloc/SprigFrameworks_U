993aee60b2d96f12c38a5edf82929125649c8995
Alexis Calvillo <acalvillo@asgbpm.com>
2021-01-23 08:00:09 -0600

    Autenticación JDBC

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,31 +1,43 @@
 package com.bolsadeideas.springboot.app;
 
-import java.nio.file.Paths;
+import org.springframework.context.annotation.Bean;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import java.nio.file.Paths;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
+	/*private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
-		WebMvcConfigurer.super.addResourceHandlers(registry);
+		super.addResourceHandlers(registry);
 		
-		String resPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
-		log.info(resPath);	
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		log.info(resourcePath);
+		
 		registry.addResourceHandler("/uploads/**")
-		.addResourceLocations(resPath);
-	}
+		.addResourceLocations(resourcePath);
+		
+	}*/
 	
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
